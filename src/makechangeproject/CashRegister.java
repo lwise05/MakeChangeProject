aed4764e0576670d3c8d575215eb0eb26e03f77b
Leslie <leslie_aldaz@hotmail.com>
2024-10-26 17:47:08 -0600

    added readMe

--- src/makechangeproject/CashRegister.java
+++ src/makechangeproject/CashRegister.java
@@ -9,9 +9,7 @@ public class CashRegister {
 		double itemAmount = 0.0;
 		double paidAmount = 0.0;
 		double changeAmount = 0.0;
-		double result = 0;
-	
-		
+		double result = 0.0;
 
 		System.out.print("Hello customer! Please enter the price of your item: ");
 		itemAmount = sc.nextDouble();
@@ -28,77 +26,68 @@ public class CashRegister {
 		}
 
 		changeAmount = paidAmount - itemAmount;
-		
-		changeAmount = round(changeAmount * 100);
-		
-		System.out.println("Your change is $" + (changeAmount / 100));
 
-		
+		changeAmount = round(changeAmount * 100);
+
+		System.out.println("");
+		System.out.println("Your change is $" + (changeAmount / 100) + " ." + " Your change will be given back in: ");
+
 //		System.out.println((int)changeAmount);
 
 		// issuing out change feature ***********************
 
-		while ((int)changeAmount > 0) {
-		if (changeAmount >= 2000) {
-			result = changeAmount / 2000;
-			changeAmount = changeAmount % 2000;
-			System.out.print("Your change back is " + (result) + " twenty dollar bill(s)");
-		}
+		while (changeAmount > 0) {
+			if (changeAmount >= 2000) {
+				result = changeAmount / 2000;
+				changeAmount = changeAmount % 2000;
+				System.out.print((int) (result) + " twenty dollar bill(s), ");
+			}
 
-		if (changeAmount >= 1000) {
-			result = changeAmount / 1000;
-			changeAmount = changeAmount % 1000;
-			System.out.print(" and " + (int) (result) + " ten dollar bill(s)");
-		}
-		if (changeAmount >= 500) {
-			result = changeAmount / 500;
-			changeAmount = changeAmount % 500;
-			System.out.print(" and " + (int) (result) + " five dollar bill(s)");
-		}
-		if (changeAmount >= 100) {
-			result = changeAmount / 100;
-			changeAmount = changeAmount % 100;
-			System.out.print(" and " + (int) (result) + " one dollar bill(s)");
-		}
-			
-		if (changeAmount >= 25) {
+			if (changeAmount >= 1000) {
+				result = changeAmount / 1000;
+				changeAmount = changeAmount % 1000;
+				System.out.print((int) (result) + " ten dollar bill(s), ");
+			}
+			if (changeAmount >= 500) {
+				result = changeAmount / 500;
+				changeAmount = changeAmount % 500;
+				System.out.print((int) (result) + " five dollar bill(s), ");
+			}
+			if (changeAmount >= 100) {
+				result = changeAmount / 100;
+				changeAmount = changeAmount % 100;
+				System.out.print((int) (result) + " one dollar bill(s), ");
+			}
+
+			if (changeAmount >= 25) {
 				result = changeAmount / 25;
 				changeAmount = changeAmount % 25;
-				System.out.print(" and " + (int) result + " quarter(s)");
+				System.out.print((int) result + " quarter(s), ");
+			}
+			if (changeAmount >= 10) {
+				result = changeAmount / 10;
+				changeAmount = changeAmount % 10;
+				System.out.print((int) result + " dime(s), ");
+			}
+			if (changeAmount >= 5) {
+				result = changeAmount / 5;
+				changeAmount = changeAmount % 5;
+				System.out.print((int) result + " nickel(s), ");
+			}
+			if (changeAmount >= 1) {
+				result = changeAmount / 1;
+				changeAmount = changeAmount % 1;
+				System.out.print((int) result + " pennie(s), ");
+				break;
+			}
 		}
-		if (changeAmount >= 10) {
-			result = changeAmount / 10;
-			changeAmount = changeAmount % 10;
-			System.out.print(" and " + (int) result + " dime(s)");
-		}
-		if (changeAmount >= 5) {
-			result = changeAmount / 5;
-			changeAmount = changeAmount % 5;
-			System.out.print(" and " + (int) result + " nickel(s)");
-		}
-		if (changeAmount >= 1) {
-			result = changeAmount / 1;
-			changeAmount = changeAmount % 1;
-			System.out.print(" and " + (int) result + " pennie(s)");
-			break;
-		}
-		
-		
-		}
-		
-		
-}
-
-
-//************************
-
-
-public static int round (double changeAmount) {
-	int round = 0;
-	if(changeAmount >=0) {
-	round = (int)(changeAmount + 0.09);	
 	}
-	return round;
-}
-}
 
+	public static int round(double changeAmount) {
+		int round = 0;
+		if (changeAmount >= 0) {
+			round = (int) (changeAmount + 0.09);
+		}
+		return round;
+	}
+}
